a100dc8a3057267fbd5c3c99e7ebbb9dab11f140
sundayliu <martinkro@aliyun.com>
2016-11-07 11:31:20 +0800

    file id

---
+++ doc/tools/file_id.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# sundayliu
+# 2016/09/21
+
+import sys
+import os
+
+
+if __name__ == "__main__":
+    print("[+] elf identifier test ...")
+    
+    print("[+] end.")
